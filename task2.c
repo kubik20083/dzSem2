5eec1fd27d85b0896adf015a8c4b53733054334b
kubik20083 <kubik20083@mail.ru>
2024-03-21 08:25:38 +0300

    task2

---
+++ task2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int num1, num2, num3, sum;
+    printf("Enter three numbers: \n");
+    scanf("%d%d%d", &num1, &num2, &num3);
+
+    printf("number sum: \n");
+    sum = num1 + num2 + num3;
+    printf("%d + %d + %d = %d\n", num1, num2, num3, sum);
+
+    printf("number multiplication: \n");
+    sum = num1 * num2 * num3;
+    printf("%d * %d * %d = %d\n", num1, num2, num3, sum);
+
+
+    return 0;
+}
