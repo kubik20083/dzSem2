72b91ed8957d541e66e3a1b406373dfca1821727
kubik20083 <kubik20083@mail.ru>
2024-03-21 08:18:09 +0300

    task1

---
+++ task1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int num1, num2, num3, sum;
+    printf("Enter three numbers: \n");
+    scanf("%d%d%d", &num1, &num2, &num3);
+    sum = num1 + num2 + num3;
+    printf("%d + %d + %d = %d\n", num1, num2, num3, sum);
+
+
+    return 0;
+}
